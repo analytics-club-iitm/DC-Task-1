c17a837c49f365a5cb0a613218b5a5b0c066c3bc
RA1809 <121611351+RA1809@users.noreply.github.com>
2023-01-03 18:46:14 +0530

    Task B edited

--- Riddhi/TaskB.py
+++ Riddhi/TaskB.py
@@ -3,48 +3,6 @@ n = int(input("Enter the list size "))
 S=int(input("target="))
 print("numbers=")
 for i in range(0, n):
-    item = int(input())
-    arr.append(item)
-
-i=0
-j=0
-c=1
-data={}
-for i in range(0,n,1):
-  for j in range(0,n,1):
-    if(arr[i]+arr[j]==S):
-      data[c]=[i,j]
-      c=c+1
-print(data)arr = []
-n = int(input("Enter the list size "))
-S=int(input("target="))
-print("numbers=")
-for i in range(0, n):
-    item = int(input())
-    arr.append(item)
-
-i=0
-j=0
-c=1
-data={}
-for i in range(0,n,1):
-  for j in range(0,n,1):
-    if(arr[i]+arr[j]==S):
-      data[c]=[i,j]
-      c=c+1
-print(data)
-arr = []
-n = int(input("Enter the list size "))
-S=int(input("target="))
-print("numbers=")
-for i in range(0, n):
-    item = int(input())
-    arr.append(item)
-arr = []
-n = int(input("Enter the list size "))
-S=int(input("target="))
-print("numbers=")
-for i in range(0, n):
     p = int(input())
     arr.append(p)
 
@@ -58,4 +16,3 @@ for i in range(0,n,1):
       data[c]=[i,j]
       c=c+1
 print(data)
-
